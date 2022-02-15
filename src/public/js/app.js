643a2408c46f7a54903b9f14d7b8e98c48bcce1a
mintchocoicecream <sxxhnjng@gmail.com>
2022-02-15 13:53:42 +0900

    addNickname commit

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,14 +1,21 @@
 const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("form");
-
+const messageForm = document.querySelector("#message");
+const nickForm = document.querySelector("#nick");
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+function makeMessage(type, payload){
+    const msg = {type, payload}
+    return JSON.stringify(msg);
+}
 
 socket.addEventListener("open", () => {
     console.log("Connected to Server ✅");
 });
 
 socket.addEventListener("message", (message) => {
-    console.log("New Message: ", message.data);
+    const li = document.createElement("li");
+    li.innerText = message.data;
+    messageList.append(li);
 });
 
 socket.addEventListener("close", () => {
@@ -19,8 +26,16 @@ socket.addEventListener("close", () => {
 function handleSubmit(event){
     event.preventDefault();
     const input = messageForm.querySelector("input");
-    socket.send(input.value);
+    socket.send(makeMessage("new_message", input.value));
+    input.value="";
+}
+
+function handleNickSubmit(event){
+    event.preventDefault();
+    const input = nickForm.querySelector("input");
+    socket.send(makeMessage("nickname", input.value));
     input.value="";
 }
 
 messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -19,10 +19,17 @@ const sockets = [];
 
 wss.on("connection", (socket) => {
     sockets.push(socket);
+    socket["nickname"] = "Anon";
     console.log("Connected to Browser ✅");
     socket.on("close",() => {console.log("Disconnected to Server ❌")});
-    socket.on("message", (message) => {
-        sockets.forEach((aSocket) => {aSocket.send(message.toString('utf8'))})
+    socket.on("message", (msg) => {
+        const message = JSON.parse(msg);
+        switch(message.type){
+            case "new_message": 
+                sockets.forEach((aSocket) => {aSocket.send(`${socket.nickname}: ${(message.payload).toString("utf8")}`)});
+            case "nickname":
+                socket["nickname"] = message.payload;
+        }
     });
 });
 
