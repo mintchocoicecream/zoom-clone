84b92ad43a567f7d2c3f72c5e7b6db28b11bb64b
mintchocoicecream <sxxhnjng@gmail.com>
2022-02-16 16:20:26 +0900

    socketIO commit
    install socketIO & connection

--- src/server.js
+++ src/server.js
@@ -1,36 +1,41 @@
 import express from "express";
+import SocketIO from "socket.io";
 import http from "http";
-import WebSocket from "ws";
 
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.redirect("/"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
 const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+const io = SocketIO(server);
 
-const sockets = [];
+io.on("connection", socket => {
+    console.log(socket);
+})
 
-wss.on("connection", (socket) => {
-    sockets.push(socket);
-    socket["nickname"] = "Anon";
-    console.log("Connected to Browser ✅");
-    socket.on("close",() => {console.log("Disconnected to Server ❌")});
-    socket.on("message", (msg) => {
-        const message = JSON.parse(msg);
-        switch(message.type){
-            case "new_message": 
-                sockets.forEach((aSocket) => {aSocket.send(`${socket.nickname}: ${(message.payload).toString("utf8")}`)});
-            case "nickname":
-                socket["nickname"] = message.payload;
-        }
-    });
-});
+// const wss = new WebSocket.Server({ server });
+// const sockets = [];
 
+// wss.on("connection", (socket) => {
+//     sockets.push(socket);
+//     socket["nickname"] = "Anon";
+//     console.log("Connected to Browser ✅");
+//     socket.on("close",() => {console.log("Disconnected to Server ❌")});
+//     socket.on("message", (msg) => {
+//         const message = JSON.parse(msg);
+//         switch(message.type){
+//             case "new_message": 
+//                 sockets.forEach((aSocket) => {aSocket.send(`${socket.nickname}: ${(message.payload).toString("utf8")}`)});
+//             case "nickname":
+//                 socket["nickname"] = message.payload;
+//         }
+//     });
+// });
+
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
 server.listen(3000, handleListen);
